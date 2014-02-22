bcd7b06072b96caba02d73e050f82eb0340df6a4
Nicholas Dedenbach <ndedenbach@hotmail.com>
2014-02-22 12:12:47 -0500

    Pause logic, etc

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public static Game that;
+
+    public bool paused = false;
+
+    void Awake () {
+        that = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Pause")) {
+            togglePause();
+        }
+        //Debug.Log(Time.timeScale);
+	}
+
+        
+    void togglePause(){
+        if (!that.paused) {
+            Time.timeScale = 0.0f;
+        }
+        else {
+            Time.timeScale = 1.0f;
+        }
+
+        that.paused = !that.paused;
+    }
+}
