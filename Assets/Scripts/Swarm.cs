303ad20e489f39bc458d045a82739a43c3608db5
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-21 23:12:20 -0500

    script setup

---
+++ Assets/Scripts/Swarm.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Swarm : MonoBehaviour {
+
+	public static Swarm that;
+	GameObject entitiesHolder;
+
+	public Sprite antibody, bloodCell_white;
+
+	List<GameObject> entities;
+
+
+	void Awake(){
+		that = this;
+		entitiesHolder = new GameObject("EntitiesHolder");
+		entities = new List<GameObject>();
+
+		for(int i=0; i < 5; ++i){
+			GameObject entity = createEntity(i);
+			entity.transform.Translate(0.5f*i, 0.5f*i, 0);
+		}
+	}
+
+	void Update(){
+	
+	}
+
+	GameObject createEntity(int num){
+		GameObject entity = new GameObject("Entity_" + num);
+		entity.AddComponent<CircleCollider2D>();
+		SpriteRenderer sprRend = entity.AddComponent<SpriteRenderer>();
+		sprRend.sprite = antibody;
+
+		entity.transform.parent = entitiesHolder.transform;
+		entities.Add(entity);
+
+		return entity;
+	}
+
+	public void move(Vector3 p){
+		for(int i=0; i < entities.Count; ++i){
+			Utils.moveToPosition(entities[i].transform, p, 2f, null);
+		}
+	}
+}
--- readme.md
+++ readme.md
@@ -8,6 +8,6 @@ Credits
 * jonwiedmann@gmail.com Jon Wiedmann (jonmann20)
 * ayarger@umich.edu Austin Yarger (ayarger)
 * ndeden@umich.edu Nicholas Dedenbach (Ryouza)
-* nmgregor@umich.edu Nickie Gregor
+* nmgregor@umich.edu Nickie Gregor (nickieg)
 * hlai@umich.edu Helen Lai (esrevni)
 * sovresat@umich.edu Sarah Ovresat (sovv)
