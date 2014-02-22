ec223843ee0cfb44adec63560debc4bc34f88b3e
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-22 14:22:47 -0500

    overworld

--- Assets/Scripts/Overworld.cs
+++ Assets/Scripts/Overworld.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Overworld : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static Overworld that;
+
+	public List<GameObject> levelLocations;
+	int cur = 0;
+	int max = 0;
+
+	bool isPressed = false;
+
+	void Awake(){
+		that = this;
+		max = levelLocations.Count - 1;
+
+		levelLocations[cur].GetComponent<SpriteRenderer>().color = Color.red;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		float hor = Input.GetAxisRaw("Horizontal");
+
+		if(!isPressed && hor != 0){
+			updateMap(hor > 0);
+		}
+
+		if(hor == 0){
+			isPressed = false;
+		}
+	}
+
+	void OnGUI(){
+		EzGUI.scaleGUI();
+
+		EzGUI.placeTxt("Level " + (cur + 1) + " selected", 57, EzGUI.FULLW - 220, 75);
+		EzGUI.blinkTxt("Press Enter", 48, EzGUI.FULLW - 220, EzGUI.HALFH);
+	}
+
+	void updateMap(bool isRight){
+		if(isRight && cur < max){	// right
+			levelLocations[cur].GetComponent<SpriteRenderer>().color = Color.white;
+			levelLocations[++cur].GetComponent<SpriteRenderer>().color = Color.red;
+			isPressed = true;
+		}
+		else if(!isRight && cur > 0){			// left
+			levelLocations[cur].GetComponent<SpriteRenderer>().color = Color.white;
+			levelLocations[--cur].GetComponent<SpriteRenderer>().color = Color.red;
+			isPressed = true;
+		}
+	}
+
+	public void updateMapByNum(int n){
+		levelLocations[cur].GetComponent<SpriteRenderer>().color = Color.white;
+		levelLocations[n].GetComponent<SpriteRenderer>().color = Color.red;
+
+		cur = n;
 	}
 }
--- readme.md
+++ readme.md
@@ -11,3 +11,14 @@ Credits
 * nmgregor@umich.edu Nickie Gregor (nickieg)
 * hlai@umich.edu Helen Lai (esrevni)
 * sovresat@umich.edu Sarah Ovresat (sovv)
+
+Inpiration
+----------
+* [Sail The Circulatory System](http://www.toxrap.org/36_kids_game1.html)
+
+Ideas
+-----
+* blood clot
+* virus attach to wall and become a spawner
+* rts game with multiple blood cell squads through out the system
+* make antibodies magnetized to viruses
