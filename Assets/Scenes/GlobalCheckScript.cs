1a512f468fa79cbbd89e60efaf079bc7b7d3a203
ayarger <austinyarger@Austin-Yargers-MacBook-Pro.local>
2014-02-23 00:09:21 -0500

    buggy

---
+++ Assets/Scenes/GlobalCheckScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalCheckScript : MonoBehaviour {
+
+	public GameObject GLOBALPrefab;
+
+	void Start () {
+		GameObject global = GameObject.Find("GLOBAL") as GameObject;
+		if(global == null)
+		{
+			Instantiate(global);
+		}
+	}
+}
